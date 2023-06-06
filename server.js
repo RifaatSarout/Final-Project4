7dc958604a96abcc9fe36335826fd4f824f94c68
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2023-06-06 11:18:59 +0300

    deployed version update-server.js

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ const connect = async () => {
   }
 };
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
